5c52639581d64a5aabfee39750a4c03e55295810
manuellybusinari <manuellybusinari@outlook.com>
2025-06-02 11:36:35 -0300

    at4

--- AppEventosPet/Models/Evento.cs
+++ AppEventosPet/Models/Evento.cs
@@ -1,29 +1,87 @@
-﻿namespace AppEventosPet.Models
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AppEventosPet.Models
 {
-    public class Evento
+    public class Evento : INotifyPropertyChanged
     {
+        private int _numeroParticipantes = 1;
+        private double _custoPorParticipante;
+        private DateTime _dataInicio = DateTime.Now;
+        private DateTime _dataTermino = DateTime.Now;
+
         public string Nome { get; set; }
         public string Tipo { get; set; }
         public string Cidade { get; set; }
-        public DateTime DataInicio { get; set; } = DateTime.Now;
-        public DateTime DataTermino { get; set; } = DateTime.Now;
-        public int NumeroParticipantes { get; set; } = 1;
-        public double CustoPorParticipante { get; set; }
 
-        public int DuracaoEmDias
+        public DateTime DataInicio
         {
-            get
+            get => _dataInicio;
+            set
             {
-                return (DataTermino - DataInicio).Days + 1;
+                if (_dataInicio != value)
+                {
+                    _dataInicio = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(DuracaoEmDias));
+                    OnPropertyChanged(nameof(CustoTotal));
+                }
             }
         }
 
-        public double CustoTotal
+        public DateTime DataTermino
         {
-            get
+            get => _dataTermino;
+            set
             {
-                return NumeroParticipantes * CustoPorParticipante;
+                if (_dataTermino != value)
+                {
+                    _dataTermino = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(DuracaoEmDias));
+                    OnPropertyChanged(nameof(CustoTotal));
+                }
             }
+        }
+
+        public int NumeroParticipantes
+        {
+            get => _numeroParticipantes;
+            set
+            {
+                if (_numeroParticipantes != value)
+                {
+                    _numeroParticipantes = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(CustoTotal));
+                }
+            }
+        }
+
+        public double CustoPorParticipante
+        {
+            get => _custoPorParticipante;
+            set
+            {
+                if (_custoPorParticipante != value)
+                {
+                    _custoPorParticipante = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(CustoTotal));
+                }
+            }
+        }
+
+        public int DuracaoEmDias => (DataTermino - DataInicio).Days + 1;
+
+        public double CustoTotal => NumeroParticipantes * CustoPorParticipante * DuracaoEmDias;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
--- AppEventosPet/Views/ResumoEvento.xaml.cs
+++ AppEventosPet/Views/ResumoEvento.xaml.cs
@@ -12,4 +12,5 @@ public partial class ResumoEvento : ContentPage
         Evento = evento;
         BindingContext = Evento;
     }
+
 }
